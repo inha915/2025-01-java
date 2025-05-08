c28bf87c1a004bd98f8dc35f84e87f6318f0101d
inha915 <dlsgk0915@gmail.com>
2025-05-08 17:38:05 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/* 클래스라는 */
+//
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
