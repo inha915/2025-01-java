469cc99d32af59a1e2fbb043ec1d4ce4bd09140d
inha915 <dlsgk0915@gmail.com>
2025-05-13 18:06:26 +0900

    for문 응용 문제 풀이

---
+++ src/ch04/ScopeStudy02.java
@@ -0,0 +1,12 @@
+package ch04;
+
+public class ScopeStudy02 {
+    public static void main(String[]arg){
+        for(int i=0; i<10; i++){
+            int i=0;
+            int sum=0;
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ src/ch04/sec04/Mission08.java
@@ -0,0 +1,13 @@
+package ch04.sec04;
+
+public class Mission08 {
+    public static void main(String[] args) {
+        // 1~100 모두 더한 값 출력
+        int sum =0;
+
+        for(int i =1; i < 101; i++) {
+            sum = sum + i;
+        }
+            System.out.println("sum: " + sum);
+    }
+}
